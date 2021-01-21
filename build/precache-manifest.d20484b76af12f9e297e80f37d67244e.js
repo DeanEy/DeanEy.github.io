707c80818c51d22b4f12327c8d7f00731b0c82d2
DeanEy <46727200+DeanEy@users.noreply.github.com>
2021-01-20 19:15:04 -0700

    Add files via upload

---
+++ build/precache-manifest.d20484b76af12f9e297e80f37d67244e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01855b2ebf8120722dd0d05d578b0af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edbf5dc4d3727b88c7d2",
+    "url": "/static/js/2.775fecdb.chunk.js"
+  },
+  {
+    "revision": "60478d08eb79cfe4448e",
+    "url": "/static/js/main.e26ea1a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
